d2feca70b06965481e98e9277d64f9b164b3b4bf
sidhanth129 <siddhanthsid12@gmail.com>
2025-06-11 23:14:47 +0530

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <ThemeProvider>
       <Router>
-        <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+        <div className=" min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
           <Header />
           <main>
             <Routes>
