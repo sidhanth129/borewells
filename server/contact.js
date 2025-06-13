3c250f017add78317353e6ac6039516b5752da8f
sidhanth129 <siddhanthsid12@gmail.com>
2025-06-13 21:32:54 +0530

    Updated contact.js

--- server/contact.js
+++ server/contact.js
@@ -13,7 +13,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://localhost:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:3001', 'https://localhost:5173'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
